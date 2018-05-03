8de5588a43a064d9566e244430f6fc1fdceb0a4c
Tian Zhang <975571814@qq.com>
2018-05-03 16:05:25 +0800

    write InputValidator test

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,32 @@
 package tw.core;
 
+import org.junit.Assert;
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import java.util.List;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+
+  @Test
+  public void validateTestWhenInputIsTrue() {
+    InputValidator inputValidator = new InputValidator();
+    String input = "1 2 3 4";
+    Boolean result = inputValidator.validate(input);
+
+    Assert.assertTrue(result);
+  }
+
+  @Test
+  public void validateTestWhenInputIsNotTrue() {
+    InputValidator inputValidator = new InputValidator();
+    String input = "11 2 3 4";
+    Boolean result = inputValidator.validate(input);
+
+    Assert.assertFalse(result);
+  }
 
 }
